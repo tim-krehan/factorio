58dc21c5ef560e93075ddacdeea99bd22d1b5825
Tim Krehan <github@timkrehan.de>
2025-01-15 20:53:51 +0100

    use env vars instead of replacing the whole script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-factoriosave="{{SAVES_DIRECTORY}}/{{WORLD_NAME}}.zip"
+factoriosave="$SAVES_DIRECTORY/$WORLD_NAME.zip"
 
 echo "save directory is '$factoriosave'"
 
 if [ ! -f "$factoriosave" ]; then
   echo "generating world"
-  exec {{BIN_DIRECTORY}}/factorio/bin/x64/factorio \
-    -map-gen-settings '{{SETTINGS_DIRECTORY}}/map-gen-setting.json' \
+  exec $BIN_DIRECTORY/factorio/bin/x64/factorio \
+    -map-gen-settings '$SETTINGS_DIRECTORY/map-gen-setting.json' \
     --create "$factoriosave"
 
-  chown {{USER}}:{{GROUP}} "$factoriosave"
+  chown $USER:$GROUP "$factoriosave"
 fi
 
-exec {{BIN_DIRECTORY}}/factorio/bin/x64/factorio \
+exec $BIN_DIRECTORY/factorio/bin/x64/factorio \
   --start-server \
-  {{SAVES_DIRECTORY}}/{{WORLD_NAME}}.zip \
+  $SAVES_DIRECTORY/$WORLD_NAME.zip \
   --server-settings \
-  {{SETTINGS_DIRECTORY}}/server-settings.json \
+  $SETTINGS_DIRECTORY/server-settings.json \
   --use-server-whitelist \
-  {{SETTINGS_DIRECTORY}}/server-whitelist.json \
+  $SETTINGS_DIRECTORY/server-whitelist.json \
   --server-adminlist \
-  {{SETTINGS_DIRECTORY}}/server-adminlist.json \
+  $SETTINGS_DIRECTORY/server-adminlist.json \
   --console-log \
-  {{LOG_DIRECTORY}}/{{WORLD_NAME}}.log \
+  $LOG_DIRECTORY/$WORLD_NAME.log \
   --verbose
